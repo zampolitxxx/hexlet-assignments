59e102f64bf4effd56153394d5009c7bc374f06f
@hexlet/cli <support@hexlet.io>
2024-01-02 17:32:14 +0900

    submit java-web-ru/mvc

---
+++ java-web-ru/mvc/src/main/java/exercise/util/NamedRoutes.java
@@ -0,0 +1,30 @@
+package exercise.util;
+
+public class NamedRoutes {
+
+    public static String rootPath() {
+        return "/";
+    }
+
+    public static String postsPath() {
+        return "/posts";
+    }
+
+    public static String buildPostPath() {
+        return "/posts/build";
+    }
+
+    public static String postPath(Long id) {
+        return postPath(String.valueOf(id));
+    }
+
+    public static String postPath(String id) {
+        return "/posts/" + id;
+    }
+
+    // BEGIN
+    public static String editPostPath(Long id) { return postPath(id) + "/edit"; }
+
+    public static String editPostPath(String id) { return postPath(id) + "/edit"; }
+    // END
+}
