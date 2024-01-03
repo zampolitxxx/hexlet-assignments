8f262f115dbb973929be9abbc052994a99f67ef1
@hexlet/cli <support@hexlet.io>
2024-01-03 15:01:42 +0900

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/App.java
@@ -0,0 +1,30 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.SessionsController;
+import exercise.util.NamedRoutes;
+
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get(NamedRoutes.rootPath(), SessionsController::root);
+        app.get(NamedRoutes.buildSessionPath(), SessionsController::build);
+        app.post(NamedRoutes.loginPath(), SessionsController::create);
+        app.post(NamedRoutes.logoutPath(), SessionsController::destroy);
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
---
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -0,0 +1,48 @@
+package exercise.controller;
+
+import java.util.Collections;
+import exercise.dto.MainPage;
+import exercise.dto.LoginPage;
+import exercise.repository.UsersRepository;
+import static exercise.util.Security.encrypt;
+
+import exercise.util.NamedRoutes;
+import exercise.util.Security;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class SessionsController {
+
+    // BEGIN
+    public static void root(Context ctx) {
+        var page = new MainPage(ctx.sessionAttribute("currentUser"));
+        ctx.render("index.jte", Collections.singletonMap("page", page));
+    }
+    public static void build(Context ctx) {
+        ctx.render("build.jte");
+    }
+    //Создаем сессию
+    public static void create(Context ctx) throws Exception {
+        try {
+            var nickname = ctx.formParamAsClass("nickname", String.class)
+                    .check(UsersRepository::existsByName, "Wrong user or password")
+                    .get();
+            var passwordHash = Security.encrypt(ctx.formParam("password"));
+            var user = UsersRepository.findByName(nickname);
+            if (user != null && user.getPassword().equals(passwordHash)) {
+                ctx.sessionAttribute("currentUser", nickname);
+                ctx.redirect("/");
+            } else throw new NotFoundResponse("Wrong username or password");
+        } catch (NotFoundResponse e) {
+            var name = ctx.formParam("name");
+            var page = new LoginPage(name, e.getMessage());
+            ctx.render("build.jte", Collections.singletonMap("page", page));
+        }
+    }
+
+    public static void destroy(Context ctx) {
+        ctx.sessionAttribute("currentUser", null);
+        ctx.redirect("/");
+    }
+    // END
+}
