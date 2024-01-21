4c27edca27d7bbf0009d078a73873095732a7eab
@hexlet/cli <support@hexlet.io>
2024-01-21 14:35:47 +0900

    submit java-spring-ru/http-crud

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,54 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import java.util.Collections;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+
+        // BEGIN
+        app.post("/users", ctx -> {
+            var firstName = ctx.formParam("firstName");
+            var lastName = ctx.formParam("lastName");
+            var email = ctx.formParam("email");
+            var password = ctx.formParam("password");
+            var user = new User(StringUtils.capitalize(firstName),
+                        StringUtils.capitalize(lastName), email.toLowerCase().trim(), Security.encrypt(password));
+                UserRepository.save(user);
+            ctx.redirect("/users");
+        });
+
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
